bb05d50f624d945b994d42a40f93fc314dfcfe52
Лач <iam@lach.pw>
2020-07-19 20:55:40 +0500

    fix: dont use ArgGroup

--- crates/jrsonnet-cli/src/manifest.rs
+++ crates/jrsonnet-cli/src/manifest.rs
@@ -23,16 +23,17 @@ impl FromStr for ManifestFormatName {
 }
 
 #[derive(Clap)]
+// TODO: Restore arg group after issue fixed in clap
 // #[clap(group = clap::ArgGroup::new("output_format"), help_heading = "MANIFESTIFICATION OUTPUT")]
 pub struct ManifestOpts {
 	/// Output format, wraps resulting value to corresponding std.manifest call.
 	/// If none - then jsonnet file is expected to return plain string value, otherwise
 	/// output will be serialized to specified format
-	#[clap(long, short = 'f', default_value = "json", possible_values = &["none", "json", "yaml"], group = "output_format")]
+	#[clap(long, short = 'f', default_value = "json", possible_values = &["none", "json", "yaml"]/*, group = "output_format"*/)]
 	format: ManifestFormatName,
 	/// Expect string as output, and write them directly.
 	/// Shortcut for --format=none, and can't be set with format together
-	#[clap(long, short = 'S', group = "output_format")]
+	#[clap(long, short = 'S'/*, group = "output_format"*/)]
 	string: bool,
 	/// Numbed of spaces to pad output manifest with.
 	/// 0 for hard tabs, -1 for single line output
