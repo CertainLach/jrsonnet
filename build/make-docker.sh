d748c856f06b2971daa6debebfb539ae3c1414fe
Yaroslav Bolyukin <iam@lach.pw>
2022-07-07 21:54:06 +0200

    ci: remove outdated build
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- build/make-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-export DOCKER_BUILDKIT=1
-docker build -t jrsonnet -f build/Dockerfile build/
-docker run --rm -it -v $PWD:/build -e CARGO_HOME=/build/cache jrsonnet:latest ash -c "cd /build&&make $@"
