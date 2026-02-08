5ac33cfd81f9dfdf39f35240e8f797120aeeb14c
Yaroslav Bolyukin <iam@lach.pw>
2026-02-08 03:11:16 +0100

    test: run go-jsonnet testsuite

--- tests/tests/cpp_test_suite.rs
+++ tests/tests/cpp_test_suite.rs
@@ -5,13 +5,12 @@ use std::{
 };
 
 use jrsonnet_evaluator::{
-	apply_tla,
+	FileImportResolver, IStr, ObjValueBuilder, State, Val, apply_tla,
 	function::TlaArg,
 	gc::WithCapacityExt as _,
 	manifest::JsonFormat,
 	rustc_hash::FxHashMap,
 	trace::{CompactFormat, PathResolver, TraceFormat},
-	FileImportResolver, IStr, ObjValueBuilder, State, Val,
 };
 use jrsonnet_stdlib::ContextInitializer;
 mod common;
@@ -21,9 +20,34 @@ fn run(file: &Path, root: &Path) -> String {
 	let mut s = State::builder();
 
 	let std_context = ContextInitializer::new(PathResolver::Relative(root.to_owned()));
+	// C++ test suite
 	std_context.add_ext_str("var1".into(), "test".into());
 	std_context
 		.add_ext_code("var2".into(), "{x:1,y:2}")
+		.expect("code is valid");
+
+	// Golang test suite
+	std_context
+		.add_ext_code("codeVar".into(), "3+3")
+		.expect("code is valid");
+	std_context.add_ext_str("stringVar".into(), "2 + 2".into());
+	std_context
+		.add_ext_code(
+			"selfRecursiveVar".into(),
+			r#"[42, std.extVar("selfRecursiveVar")[0] + 1]"#,
+		)
+		.expect("code is valid");
+	std_context
+		.add_ext_code(
+			"mutuallyRecursiveVar1".into(),
+			r#"[42, std.extVar("mutuallyRecursiveVar2")[0] + 1]"#,
+		)
+		.expect("code is valid");
+	std_context
+		.add_ext_code(
+			"mutuallyRecursiveVar2".into(),
+			r#"[42, std.extVar("mutuallyRecursiveVar1")[0] + 1]"#,
+		)
 		.expect("code is valid");
 
 	s.context_initializer((std_context, TestContextInitializer))
@@ -64,6 +88,11 @@ fn run(file: &Path, root: &Path) -> String {
 			}),
 		);
 		v = apply_tla(&args, v).expect("failed to apply tla");
+	} else {
+		v = match apply_tla(&FxHashMap::new(), v) {
+			Ok(v) => v,
+			Err(e) => return trace_format.format(&e).unwrap(),
+		};
 	}
 
 	match v.manifest(JsonFormat::default()) {
@@ -81,11 +110,14 @@ fn read_file(path: &Path) -> io::Result<Option<String>> {
 }
 
 const SKIPPED: &[&str] = &[
+	// C++ tests:
+
 	// Parser fails with stack overflow. While is a bug, this is a too unusual
 	// thing to run untrusted jsonnet code? Will be fixed with nom/rowan.
 	"error.parse.deep_array_nesting.jsonnet",
-	// Runtime error in jrsonnet
+	// Runtime, not static error in jrsonnet
 	"error.parse.object_local_clash.jsonnet",
+	"error.function_duplicate_param.jsonnet",
 	// Too slow to throw due to how lazyness is implemented in jrsonnet
 	"error.recursive_object_non_term.jsonnet",
 	// In jrsonnet returns the one passed argument, works as Rust's dbg!()
@@ -96,17 +128,73 @@ const SKIPPED: &[&str] = &[
 	"invariant_manifest.jsonnet",
 	// Little bit hard to capture trace logs in this test suite at this moment
 	"trace.jsonnet",
+	// Go tests:
+
+	// Something is wrong, go-jsonnet skips safe integer range check here
+	"bitwise_or9.jsonnet",
+	// Jrsonnet does not use byte strings, all utf8 is converted to bytes first
+	"builtinBase64_string_high_codepoint.jsonnet",
+	// Split by empty string is string characters, same as everywhere else
+	"builtinSplitLimitR6.jsonnet",
+	// escapeStringJson only accepts string in jrsonnet
+	"builtin_escapeStringJson.jsonnet",
+	// golang float formatting is inefficient and not portable
+	"builtin_manifestTomlEx.jsonnet",
+	"div3.jsonnet",
+	"pow6.jsonnet",
+	// golang escapes "e" yaml key, does it think it is float?
+	"builtin_manifestYamlDoc.jsonnet",
+	// Wtf?..
+	// Result
+	// [
+	//   {},
+	//   {},
+	//   []
+	// ]
+	// and golden
+	// [
+	//   {},
+	//   {},
+	//   []
+	// ]
+	// did not match structurally:
+	//  [
+	//    ...
+	// -  {
+	// -  }
+	// +  {
+	// +  }
+	//    [
+	//    ]
+	//  ]
+	"empty_object_comp.jsonnet",
+	"object_hidden.jsonnet",
+	// multi output is a CLI part, not an interpreter.
+	"multi.jsonnet",
+	"multi_no_newline.jsonnet",
+	"multi_no_newline_string_output.jsonnet",
+	"multi_string_output.jsonnet",
+	// Tested otherwise
+	"native1.jsonnet",
+	"native2.jsonnet",
+	"native3.jsonnet",
+	"native6.jsonnet",
+	// Since when parser should throw an error for that?..
+	"number_leading_zero.jsonnet",
+	// Jrsonnet has this overload
+	"number_times_string.jsonnet",
 ];
 
 #[test]
 fn cpp_test_suite() -> io::Result<()> {
 	use json_structural_diff::JsonDiff;
 
-	let root_tests = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-	let root = root_tests.join("cpp_test_suite");
-	let root_override = root_tests.join("cpp_test_suite_golden_override");
+	for root_dir in ["cpp_test_suite", "go_testdata"] {
+		let root_tests = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+		let root = root_tests.join(root_dir);
+		let root_override = root_tests.join(format!("{root_dir}_golden_override"));
 
-	for entry in fs::read_dir(&root).map_err(|e| io::Error::new(ErrorKind::Other, format!("failed to enumerate cpp_test_suite dir (Note: it needs to be cloned from C++ jsonnet repo for this test): {e}")))? {
+		for entry in fs::read_dir(&root).map_err(|e| io::Error::new(ErrorKind::Other, format!("failed to enumerate cpp_test_suite dir (Note: it needs to be cloned from C++ jsonnet repo for this test): {e}")))? {
 		let entry = entry?;
 		if !entry.path().extension().map_or(false, |e| e == "jsonnet") {
 			continue;
@@ -125,15 +213,26 @@ fn cpp_test_suite() -> io::Result<()> {
 
 		let mut golden_path = entry.path();
 		golden_path.set_extension("jsonnet.golden");
+
+		let mut golden_path2 = entry.path();
+		golden_path2.set_extension("golden");
+
 		let golden_override =
 			root_override.join(&golden_path.file_name().expect("file has basename"));
 
+		// .jsonnet.golden for C++ tests
 		let mut golden = read_file(&golden_path)?;
+		// .golden for Go tests
+		if golden.is_none() && let Some(golden_path) = read_file(&dbg!(golden_path2))? {
+			golden = Some(golden_path);
+		}
 
+		// Any of them can be overriden by overrides
 		if let Some(golden_path) = read_file(&golden_override)? {
 			golden = Some(golden_path);
 		}
 
+		// Otherwise assume test should just not fail and return true.
 		let golden = golden.unwrap_or_else(|| "true".to_owned());
 
 		match (serde_json::from_str(&result), serde_json::from_str(&golden)) {
@@ -171,6 +270,7 @@ fn cpp_test_suite() -> io::Result<()> {
 				}
 			}
 		};
+	}
 	}
 
 	Ok(())
