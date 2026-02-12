3421cac4341abf00c833d0dfb4d0781219cd06d9
Yaroslav Bolyukin <iam@lach.pw>
2026-02-12 17:55:07 +0100

    test: use insta for jrsonnet own golden testsuite

--- tests/tests/golden.rs
+++ tests/tests/golden.rs
@@ -1,8 +1,6 @@
-use std::{
-	fs, io,
-	path::{Path, PathBuf},
-};
+use std::path::Path;
 
+use insta::{assert_snapshot, glob};
 use jrsonnet_evaluator::{
 	FileImportResolver, State,
 	manifest::JsonFormat,
@@ -40,66 +38,10 @@ fn run(file: &Path) -> String {
 }
 
 #[test]
-fn test() -> io::Result<()> {
-	use json_structural_diff::JsonDiff;
+fn golden() {
+	glob!("../", "golden/*.jsonnet", |path| {
+		let result = run(&path);
 
-	let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-	root.push("golden");
-
-	for entry in fs::read_dir(&root)? {
-		let entry = entry?;
-		if !entry.path().extension().map_or(false, |e| e == "jsonnet") {
-			continue;
-		}
-
-		let result = run(&entry.path());
-
-		let mut golden_path = entry.path();
-		golden_path.set_extension("jsonnet.golden");
-
-		if !golden_path.exists() {
-			fs::write(golden_path, &result)?;
-			continue;
-		}
-
-		let golden = fs::read_to_string(golden_path)?;
-
-		match (serde_json::from_str(&result), serde_json::from_str(&golden)) {
-			(Err(_), Ok(_)) => assert_eq!(
-				result,
-				golden,
-				"unexpected error for golden {}",
-				entry.path().display()
-			),
-			(Ok(_), Err(_)) => assert_eq!(
-				result,
-				golden,
-				"expected error for golden {}",
-				entry.path().display()
-			),
-			(Ok(result), Ok(golden)) => {
-				// Show diff relative to golden`.
-				let diff = JsonDiff::diff_string(&golden, &result, false);
-				if let Some(diff) = diff {
-					panic!(
-						"Result \n{result:#}\n\
-							and golden \n{golden:#}\n\
-							did not match structurally:\n{diff:#}\n\
-							for golden {}",
-						entry.path().display()
-					);
-				}
-			}
-			(Err(_), Err(_)) => {}
-		};
-
-		assert_eq!(
-			result,
-			golden,
-			"golden didn't match for {}",
-			entry.path().display()
-		);
-	}
-
-	Ok(())
+		assert_snapshot!(result)
+	});
 }
